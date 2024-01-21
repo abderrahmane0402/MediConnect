eb36ed7abc8c3215fabf496d9ef79fcf23118dab
abderrahmanesabkari <abderrahmanesabkari@gmail.com>
2024-01-21 23:25:05 +0100

    finish the Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -13,30 +13,27 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 export default function Header() {
 
     return (
-        <header className="sticky w-full h-auto flex justify-between items-center px-14">
-            <div className="flex gap-5">
-                <h3 className="text-2xl tracking-wide font-extrabold">
+        <header className="sticky w-full h-auto flex justify-between items-center px-14 py-2 border-b-2">
+            <div className="flex gap-5 items-center">
+                <h3 className="text-xl tracking-wide font-bold">
                     MediConnect
                 </h3>
                 <Nav />
             </div>
             <div className="flex flex-row-reverse">
                 <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" size="icon">
-                            <Avatar>
-                                <AvatarImage src="https://github.com/shadcn.png" />
-                                <AvatarFallback>CN</AvatarFallback>
-                            </Avatar>
-                        </Button>
+                    <DropdownMenuTrigger>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
                     </DropdownMenuTrigger>
                     <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuLabel>Mon Compte</DropdownMenuLabel>
                         <DropdownMenuSeparator />
                         <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>déconnexion</DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
             </div>
--- components/shared/Nav.tsx
+++ components/shared/Nav.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import Link from "next/link"
+import * as React from "react";
+import Link from "next/link";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -12,9 +12,7 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-
-
+} from "@/components/ui/navigation-menu";
 
 export default function Nav() {
   return (
@@ -31,22 +29,37 @@ export default function Nav() {
           <NavigationMenuTrigger>Dossier Medical</NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              <li className="w-full h-full p-4 rounded-lg hover:bg-blue-100/70">
-                <Link href="/dossierMedical">
-                  <div className="text-base font-medium leading-none">Consulter les dossiers medical</div>
-                  <p className="mt-1 line-clamp-2 text-sm leading-snug text-muted-foreground">
-                    cette page contient une list des dossiers medical
-                  </p>
-                </Link>
-              </li>
-              <li className="w-full h-full p-4 rounded-lg hover:bg-blue-100/70">
-                <Link href="/DossierMedical/ajouter">
-                  <div className="text-base font-medium leading-none">Ajouter un nouveau dossiers medical</div>
-                  <p className="mt-1 line-clamp-2 text-sm leading-snug text-muted-foreground">
-                    cette page demande les informations pour enregistrer un nouveau dossier medical
-                  </p>
-                </Link>
-              </li>
+              <ListItem href="/dossierMedical" title="les dossiers medicals">
+                cette page contient une list des dossiers medicals
+              </ListItem>
+              <ListItem
+                href="/dossierMedical/ajouter"
+                title="Ajouter dossiers medical"
+              >
+                cette page demande les informations pour enregistrer un nouveau
+                dossier medical
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>cabinet médical</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+              <ListItem href="/employee" title="Les employee">
+                cette page contient une list des employees du cabinet
+              </ListItem>
+              <ListItem href="/employee/ajouter" title="Ajouter Employee">
+                cette page demande les informations pour enregistrer un nouveau
+                employee
+              </ListItem>
+              <ListItem href="/materiel" title="materiel du cabinet">
+                cette page affiche les informations sur le materiel du caninet
+              </ListItem>
+              <ListItem href="/materiel/ajouter" title="ajouter un materiel">
+                cette page demande les informations pour enregistrer un nouveau
+                materiel
+              </ListItem>
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
@@ -59,6 +72,26 @@ export default function Nav() {
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
-  )
+  );
 }
 
+function ListItem({
+  href,
+  title,
+  children,
+}: {
+  href: string;
+  title: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <li className="w-full h-full p-4 rounded-lg hover:bg-blue-100/70 transition ease-linear delay-75">
+      <Link href={href}>
+        <div className="text-base font-medium leading-none">{title}</div>
+        <p className="mt-1 line-clamp-2 text-sm leading-snug text-muted-foreground">
+          {children}
+        </p>
+      </Link>
+    </li>
+  );
+}
