af4e7d89b319b43ee1771d0faa120e27e31bcf6e
ZakariaRekhla2004 <zakariaest49@gmail.com>
2024-02-22 11:28:42 +0100

    i m just add pictuere of healthy  minsiter

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,9 +6,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import profile from "../../public/materiel/ministere-de-la-sante-maroc-logo.png";
 import Nav from "./Nav";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 
 const MobileNav = dynamic(() => import("./MobileNav"), { ssr: false });
 
@@ -16,9 +18,14 @@ export default function Header() {
   return (
     <header className="sticky w-full h-auto flex justify-between items-center px-1 md:px-4 lg:px-14 py-1 md:py-2 border-b bg-white shadow-sm">
       <div className="flex gap-5 items-center">
+      <Image src={profile} alt={"vv"} 
+        width={50}      
+        height={50}       
+            />
         <h3 className="text-base md:text-lg lg:text-xl tracking-wide font-bold text-blue-600">
           MediConnect
         </h3>
+        
         <Nav />
       </div>
       <div className="flex flex-row-reverse">
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    loader: 'custom',
+    loaderFile: './lib/my_loader.ts',
+    unoptimized: true,
+  },
 };
 
+
 export default nextConfig;
+
